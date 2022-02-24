706868c37071d7ff3421010b32e01fde422f0887
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2022-02-24 11:12:56 +0900

    generate.py

--- pix2pix/generate.py
+++ pix2pix/generate.py
@@ -7,10 +7,12 @@ from matplotlib import pyplot as plt
 
 import datetime
 
+from tensorflow.python.training.checkpoint_utils import checkpoints_iterator
+
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+
 PATH = './images/dataset/pix2pix/'
 checkpoint_dir = './checkpoints/pix2pix'
-
-EPOCHS = 1000
 
 BUFFER_SIZE = 400
 BATCH_SIZE = 1
@@ -32,13 +34,6 @@ def load(image_file):
 
     return input_image, real_image
 
-inp, re = load(PATH+'test/content0.jpg')
-
-plt.figure()
-plt.imshow(inp/255.0)
-plt.figure()
-plt.imshow(re/255.0)
-
 def resize(input_image, real_image, height, width):
     input_image = tf.image.resize(input_image, [height, width],
                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
@@ -47,41 +42,9 @@ def resize(input_image, real_image, height, width):
 
     return input_image, real_image
 
-def random_crop(input_image, real_image):
-    stacked_image = tf.stack([input_image, real_image], axis=0)
-    cropped_image = tf.image.random_crop(
-        stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
-
-    return cropped_image[0], cropped_image[1]
-
-
 def normalize(input_image, real_image):
     input_image = (input_image / 127.5) - 1
     real_image = (real_image / 127.5) - 1
-
-    return input_image, real_image
-
-@tf.function()
-def random_jitter(input_image, real_image):
-    input_image, real_image = resize(input_image, real_image, 286, 286)
-    input_image, real_image = random_crop(input_image, real_image)
-    if tf.random.uniform(()) > 0.5:
-        input_image = tf.image.flip_left_right(input_image)
-        real_image = tf.image.flip_left_right(real_image)
-    return input_image, real_image
-
-plt.figure(figsize=(6, 6))
-for i in range(4):
-    rj_inp, rj_re = random_jitter(inp, re)
-    plt.subplot(2, 2, i+1)
-    plt.imshow(rj_inp/255.0)
-    plt.axis('off')
-plt.show()
-
-def load_image_train(image_file):
-    input_image, real_image = load(image_file)
-    input_image, real_image = random_jitter(input_image, real_image)
-    input_image, real_image = normalize(input_image, real_image)
 
     return input_image, real_image
 
@@ -92,12 +55,6 @@ def load_image_test(image_file):
     input_image, real_image = normalize(input_image, real_image)
 
     return input_image, real_image
-
-train_dataset = tf.data.Dataset.list_files(PATH + 'train/*.jpg')
-train_dataset = train_dataset.map(load_image_train,
-                                  num_parallel_calls=tf.data.AUTOTUNE)
-train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.batch(BATCH_SIZE)
 
 test_dataset = tf.data.Dataset.list_files(PATH + 'test/*.jpg')
 test_dataset = test_dataset.map(load_image_test)
@@ -117,10 +74,6 @@ def downsample(filters, size, apply_batchnorm=True):
 
     return result
 
-down_model = downsample(3, 4)
-down_result = down_model(tf.expand_dims(inp, 0))
-print (down_result.shape)
-
 def upsample(filters, size, apply_dropout=False):
     initializer = tf.random_normal_initializer(0., 0.02)
     result = tf.keras.Sequential()
@@ -137,8 +90,6 @@ def upsample(filters, size, apply_dropout=False):
     return result
 
 up_model = upsample(3, 4)
-up_result = up_model(down_result)
-print (up_result.shape)
 
 def Generator():
     inputs = tf.keras.layers.Input(shape=[256,256,3])
@@ -186,16 +137,8 @@ def Generator():
 
 generator = Generator()
 tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64)
-gen_output = generator(inp[tf.newaxis,...], training=False)
-#plt.imshow(gen_output[0,...])
 
 LAMBDA = 100
-
-def generator_loss(disc_generated_output, gen_output, target):
-    gan_loss = loss_object(tf.ones_like(disc_generated_output), disc_generated_output)
-    l1_loss = tf.reduce_mean(tf.abs(target - gen_output))
-    total_gen_loss = gan_loss + (LAMBDA * l1_loss)
-    return total_gen_loss, gan_loss, l1_loss
 
 def Discriminator():
     initializer = tf.random_normal_initializer(0., 0.02)
@@ -219,17 +162,7 @@ def Discriminator():
 discriminator = Discriminator()
 tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=64)
 
-disc_out = discriminator([inp[tf.newaxis,...], gen_output], training=False)
-plt.imshow(disc_out[0,...,-1], vmin=-20, vmax=20, cmap='RdBu_r')
-plt.colorbar()
-
 loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-
-def discriminator_loss(disc_real_output, disc_generated_output):
-    real_loss = loss_object(tf.ones_like(disc_real_output), disc_real_output)
-    generated_loss = loss_object(tf.zeros_like(disc_generated_output), disc_generated_output)
-    total_disc_loss = real_loss + generated_loss
-    return total_disc_loss
 
 generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
 discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
@@ -253,83 +186,6 @@ def generate_images(model, test_input, tar, num):
         plt.axis('off')
     resultFig.savefig(PATH + 'result/result' + str(num) + '.png')
 
-def generate_image_fromPicture(model, test_input, tar):
-    prediction = model(test_input, training=True)
-    plt.figure(figsize=(15,15))
-    display_list = [test_input[0], tar[0], prediction[0]]
-    title = ['Input Image', 'Ground Truth', 'Predicted Image']
-
-    for i in range(3):
-        plt.subplot(1, 3, i+1)
-        plt.title(title[i])
-        # getting the pixel values between [0, 1] to plot it.
-        plt.imshow(display_list[i] * 0.5 + 0.5)
-        plt.axis('off')
-    plt.show()
-
-for example_input, example_target in test_dataset.take(1):
-    generate_images(generator, example_input, example_target, 0)
-
-log_dir="logs/"
-
-summary_writer = tf.summary.create_file_writer(
-  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-
-@tf.function
-def train_step(input_image, target, epoch):
-    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-        gen_output = generator(input_image, training=True)
-
-        disc_real_output = discriminator([input_image, target], training=True)
-        disc_generated_output = discriminator([input_image, gen_output], training=True)
-
-        gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
-        disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
-
-    generator_gradients = gen_tape.gradient(gen_total_loss,
-                                            generator.trainable_variables)
-    discriminator_gradients = disc_tape.gradient(disc_loss,
-                                                discriminator.trainable_variables)
-
-    generator_optimizer.apply_gradients(zip(generator_gradients,
-                                            generator.trainable_variables))
-    discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
-                                                discriminator.trainable_variables))
-
-    with summary_writer.as_default():
-        tf.summary.scalar('gen_total_loss', gen_total_loss, step=epoch)
-        tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=epoch)
-        tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=epoch)
-        tf.summary.scalar('disc_loss', disc_loss, step=epoch)
-
-def fit(train_ds, epochs, test_ds):
-    for epoch in range(epochs):
-        start = time.time()
-
-        #display.clear_output(wait=True)
-        print("Epoch: ", epoch)
-
-        # Train
-        for n, (input_image, target) in train_ds.enumerate():
-            print('.', end='')
-            if (n+1) % 100 == 0:
-                print()
-            train_step(input_image, target, epoch)
-        print()
-
-        # saving (checkpoint) the model every 20 epochs
-        if (epoch + 1) % 100 == 0:
-            checkpoint.save(file_prefix = checkpoint_prefix)
-            for example_input, example_target in test_dataset.take(1):
-                generate_images(generator, example_input, example_target, epoch)
-
-        print ('Time taken for epoch {} is {} sec\n'.format(epoch + 1,
-                                                            time.time()-start))
-
-    checkpoint.save(file_prefix = checkpoint_prefix)
-
-fit(train_dataset, EPOCHS, test_dataset)
-
 checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
-for inp, tar in test_dataset.take(50):
-    generate_images(generator, inp, tar)
+for num, img in enumerate(test_dataset.take(50)):
+    generate_images(generator, img[0], img[1], num)
